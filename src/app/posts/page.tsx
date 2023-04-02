5b300eb3c34ad94300599f6e6b20b1a3ad0af772
harseille <joonhabaak@gmail.com>
2023-04-03 01:08:42 +0900

    :sparkles: get notion api

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,27 @@
-const PostListPage = () => {
-  return <div>PostPage</div>;
+import getItem from '@/services/getItem';
+import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+
+const PostListPage = async () => {
+  const { results } = await getItem();
+
+  console.log(
+    results.map((item) => {
+      console.log('=========================');
+      console.log(item);
+    })
+  );
+
+  const category = new Set(
+    results.map(({ properties }: any) => properties.category.select.name)
+  );
+
+  return (
+    <div>
+      {[...category.values()].map((item) => (
+        <div key={item}>{item}</div>
+      ))}
+    </div>
+  );
 };
 
 export default PostListPage;
