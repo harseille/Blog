2441a52de20bacdf7af04d6b9a52a7e5b5c5c9c2
Joonha Park <85827017+harseille@users.noreply.github.com>
2023-04-03 01:26:31 +0900

    Dev (#6)
    * Feature/#1 (#2)
    
    * :heavy_plus_sign: TailwindCSS 추가
    
    * :heavy_plus_sign: Recoil 추가
    
    * Feature/#4 notion api (#5)
    
    * Feature/#1 (#2) (#3)
    
    * :heavy_plus_sign: TailwindCSS 추가
    
    * :heavy_plus_sign: Recoil 추가
    
    * :lipstick: main page styling
    
    * :chore: set tanstack-query
    
    * :sparkles: get notion api
    
    ---------
    
    Co-authored-by: Joonha Park <85827017+harseille@users.noreply.github.com>
    Co-authored-by: harseille <joonhabaak@gmail.com>
    
    ---------
    
    Co-authored-by: Joonha Park <127076460+joonha-park-switchwon@users.noreply.github.com>

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,20 @@
-const PostListPage = () => {
-  return <div>PostPage</div>;
+import getItem from '@/services/getItem';
+import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+
+const PostListPage = async () => {
+  const { results } = await getItem();
+
+  const category = new Set(
+    results.map(({ properties }: any) => properties.category.select.name)
+  );
+
+  return (
+    <div>
+      {[...category.values()].map((item) => (
+        <div key={item}>{item}</div>
+      ))}
+    </div>
+  );
 };
 
 export default PostListPage;
